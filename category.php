68e86ea567b395f4f8c1fd475002aa8c310cedf1
hasanamiin24-cmd <hasanamiin24@gmail.com>
2026-08-08 17:28:47 +0300

    updated

--- category.php
+++ category.php
@@ -51,6 +51,14 @@ $current_category = get_category($conn, $id);
 		    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		      <span class="navbar-toggler-icon"></span>
 		    </button>
+			   <div class="collapse navbar-collapse" 
+		         id="navbarSupportedContent">
+		      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+		        <li class="nav-item">
+		          <a class="nav-link active" 
+		             aria-current="page" 
+		             href="index.php">Home</a>
+		        </li>
 <li class="nav-item">
     <a class="nav-link" href="contact.php">
         <i class="fas fa-phone"></i> Contact
