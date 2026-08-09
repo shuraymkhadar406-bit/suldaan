1a5b718144d83d178596a81ff9f1b3c05ffc2ad9
hasanamiin24-cmd <hasanamiin24@gmail.com>
2026-08-09 07:54:47 +0300

    updated

--- about.php
+++ about.php
@@ -37,12 +37,7 @@
     <div class="row">
 
         <div class="col-md-6">
-<<<<<<< HEAD
             <h2>About Our System Worked</h2>
-=======
-            <h2>About Our System Worked & Worked @ vv</h2>
->>>>>>> 7dbeb8c655ba9cd6e98db38cf33e6b547bdb0bad
-
             <p>
                 The Digital Library & E-Book Store is an online platform
                 that allows users to search, read, and download books
